3d67ad8ecf932293a20d4085f3375c8808e81941
Ben Platta <bmplatta@gmail.com>
2021-04-27 16:52:37 -0500

    update convert script

--- convert.py
+++ convert.py
@@ -3,6 +3,9 @@ import os
 import pathlib
 import yaml
 
+from datetime import datetime
+from typing import List, Dict
+
 
 PLUGINS_DIR = "plugins"
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -15,17 +18,43 @@ class Constants:
     OutputFormatYaml = "json"
 
 
+def log(message):
+    print("[%s] [%s]" % (datetime.utcnow().isoformat()[:-3], message))
+
+
 def get_plugins_path():
     return os.path.join(PROJECT_PATH, PLUGINS_DIR)
 
 
-def gather_plugin_directories(path):
+def gather_plugin_directories(path) -> List[str]:
+    """Get list of directories for plugins
+
+    Args:
+        path (str): path to plugins directory
+
+    Returns:
+        List[str]
+    """
     return [
         os.path.join(path, obj) for obj in os.listdir(path) if os.path.isdir(os.path.join(path, obj))
     ]
 
 
-def load_plugin_yaml(plugin_directory, should_raise=False):
+def load_plugin_yaml(plugin_directory, should_raise=False) -> Dict:
+    """Load plugin's yaml file from directory.
+    
+    Raise FileNotFoundError if no file found.
+
+    Args:
+        plugin_directory (str]): path to plugin dir
+        should_raise (bool, optional)
+
+    Raises:
+        FileNotFoundError:
+
+    Returns:
+        Dict
+    """
     plugin_data_path = os.path.join(plugin_directory, PLUGIN_DATA_FILE)
     if not pathlib.Path(plugin_data_path).exists():
         if should_raise:
@@ -35,6 +64,19 @@ def load_plugin_yaml(plugin_directory, should_raise=False):
 
 
 def write_outfile(data, filename=Constants.OutFile, output_format=Constants.OutputFormatJson):
+    """Write data as output format to file
+
+    Args:
+        data (dict): data to write
+        filename (str, optional): defaults to catalog.[format]
+        output_format (str, optional): json or yaml. defaults to json
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        [type]: [description]
+    """
     full_filename = "%s.%s" % (filename, output_format)
     with open(full_filename, 'w') as outfile:
         if output_format == Constants.OutputFormatJson:
@@ -47,10 +89,14 @@ def write_outfile(data, filename=Constants.OutFile, output_format=Constants.Outp
 
 
 if __name__ == "__main__":
-    print("HERE WE GO")
+    log("Starting gather plugins")
     plugins_dir_path = get_plugins_path()
+    log("Reading plugins from %s" % plugins_dir_path)
     plugin_directories = gather_plugin_directories(plugins_dir_path)
+    log("Found %d plugin directories" % (len(plugin_directories)))
     plugins_data = [
         load_plugin_yaml(plugin_dir, should_raise=True) for plugin_dir in plugin_directories
     ]
-    write_outfile(plugins_data)
+    output_file = "plugin-catalog"
+    log("Writing output to file %s" % )
+    write_outfile(plugins_data, filename=output_file, output_format="json")
